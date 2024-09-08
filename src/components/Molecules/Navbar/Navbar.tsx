9eb5aee881087f5c2fc31496e7cdd1d68e4d0dad
Vivek Maurya <Vivek563maurya@gmail.com>
2024-09-08 23:04:40 +0530

    fix: home route

--- src/components/Molecules/Navbar/Navbar.tsx
+++ src/components/Molecules/Navbar/Navbar.tsx
@@ -36,7 +36,10 @@ function Navbar() {
                <nav id="nav" className={` ${isNavbarFixed ? "navbar-fixed" : ""} `}>
                     <div className="nav-center">
                          <div className="nav-header">
-                              <Image width={100} height={100} style={{ width: "336px", height: "70px" }} src="/logo.svg" alt="nav-logo" className="nav-logo" />
+                              <a href="/" className="nav-link">
+                                   <Image width={100} height={100} style={{ width: "336px", height: "70px" }} src="/logo.svg" alt="nav-logo" className="nav-logo" />
+                              </a>
+
                               <button type="button" className="nav-btn" onClick={openSidebar} id="nav-btn">
                                    <i className="fa-solid fa-bars-staggered"></i>
                               </button>
