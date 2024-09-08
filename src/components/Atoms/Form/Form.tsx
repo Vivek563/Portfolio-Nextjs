4a298c911d0f83f6f3b9568c8a2f7f85934a4835
Vivek Maurya <Vivek563maurya@gmail.com>
2024-09-08 12:22:31 +0530

    Fix: form input

--- src/components/Atoms/Form/Form.tsx
+++ src/components/Atoms/Form/Form.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { toast } from "sonner";
+"use client";
 
-function Form() {
-     const cols = 30;
-     const rows = 5;
+import React, { useState } from "react";
+import Card from "./card";
 
+interface FeedbackFormProps {}
+
+const FeedbackForm: React.FC<FeedbackFormProps> = () => {
      const [status, setStatus] = useState<string | null>(null);
      const [error, setError] = useState<string | null>(null);
 
@@ -39,32 +40,49 @@ function Form() {
           }
      };
 
-     if (status === "ok") {
-          toast.success("Form Submitted, We will connect Soon");
-     }
-     if (status === "error") {
-          toast.success("Failed to Submit the form");
-     }
-
      return (
-          <>
-               <div className="container">
-                    <input type="hidden" name="form-name" value="feedback" />
-                    <div className="contact-form-container">
-                         <form className="contact-form" onSubmit={handleFormSubmit} name="feedback">
-                              <input type="hidden" name="form-name" value="contact" />
-                              <span className="form-header">Contact us</span>
-                              <input type="text" className="email-input" placeholder="Name" id="name" name="name" required />
-                              <input type="email" id="email" name="email" className="email-input" placeholder="Email" required />
-                              <textarea cols={cols} rows={rows} id="message" name="message" className="message" placeholder="Message..." required />
-                              <button className="submit" type="submit" value="Submit message">
-                                   Submit
-                              </button>
-                         </form>
-                    </div>
-               </div>
-          </>
+          <div className="w-full md:max-w-md">
+               <Card title="Leave Feedback">
+                    <form name="feedback" onSubmit={handleFormSubmit} className="text-black flex flex-col gap-3 align-center">
+                         <input type="hidden" name="form-name" value="feedback" />
+                         <input name="name" type="text" placeholder="Name" required className="input input-bordered" />
+                         <input name="email" type="email" placeholder="Email (optional)" className="input input-bordered" />
+                         <input name="message" type="text" placeholder="Message" required className="input input-bordered" />
+                         <button className="btn btn-primary" type="submit" disabled={status === "pending"}>
+                              Submit
+                         </button>
+                         {status === "ok" && (
+                              <div className="alert alert-success">
+                                   <SuccessIcon />
+                                   Submitted!
+                              </div>
+                         )}
+                         {status === "error" && (
+                              <div className="alert alert-error">
+                                   <ErrorIcon />
+                                   {error}
+                              </div>
+                         )}
+                    </form>
+               </Card>
+          </div>
      );
-}
+};
 
-export default Form;
+const SuccessIcon: React.FC = () => {
+     return (
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+     );
+};
+
+const ErrorIcon: React.FC = () => {
+     return (
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+     );
+};
+
+export default FeedbackForm;
